4089e22efd0ba3890b764cb0c1e25d9339af8e5e
M6Irinel <momoloacairinel666@gmail.com>
2022-10-19 22:12:22 +0200

    add visual errors form

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -9,19 +9,29 @@
 
                 <div class="form-group">
                     <label for="title">Title</label>
-                    <input type="text" class="form-control" id="title" name="title"
-                        value="{{ old('title') }}">
+                    <input type="text" class="form-control @error('title') is-invalid @enderror" id="title"
+                        name="title" value="{{ old('title') }}">
+                    @error('title')
+                        <p class="invalid-feedback">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="form-group">
                     <label for="description">Description</label>
-                    <textarea class="form-control" id="description" name="description" rows="3">{{ old('description') }}</textarea>
+                    <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description"
+                        rows="3">{{ old('description') }}</textarea>
+                    @error('description')
+                        <p class="invalid-feedback">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="form-group">
                     <label for="shug">Shug</label>
-                    <input type="text" class="form-control" id="shug" name="shug"
-                        value="{{ old('shug') }}">
+                    <input type="text" class="form-control @error('shug') is-invalid @enderror" id="shug"
+                        name="shug" value="{{ old('shug') }}">
+                    @error('shug')
+                        <p class="invalid-feedback">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="d-flex justify-content-center">
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -3,22 +3,34 @@
 @section('content')
     <section>
         <div class="container">
-            <form action="{{route('admin.posts.update', $post)}}" method="POST">
+            <form action="{{ route('admin.posts.update', $post) }}" method="POST">
 
                 @csrf
                 @method('PUT')
 
                 <div class="form-group">
                     <label for="title">Title</label>
-                    <input type="text" class="form-control" id="title" name="title" value="{{ old('title', $post->title) }}">
+                    <input type="text" class="form-control @error('title') is-invalid @enderror" id="title"
+                        name="title" value="{{ old('title', $post->title) }}">
+                    @error('title')
+                        <p class="invalid-feedback">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <label for="description">Description</label>
-                    <textarea class="form-control" id="description" name="description" rows="3">{{ old('description', $post->description) }}</textarea>
+                    <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description"
+                        rows="3">{{ old('description', $post->description) }}</textarea>
+                    @error('description')
+                        <p class="invalid-feedback">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <label for="shug">Shug</label>
-                    <input type="text" class="form-control" id="shug" name="shug" value="{{ old('shug', $post->shug) }}">
+                    <input type="text" class="form-control @error('shug') is-invalid @enderror" id="shug"
+                        name="shug" value="{{ old('shug', $post->shug) }}">
+                    @error('shug')
+                        <p class="invalid-feedback">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="d-flex justify-content-center">
                     <input class="btn btn-secondary" type="submit" value="UPDATE">
